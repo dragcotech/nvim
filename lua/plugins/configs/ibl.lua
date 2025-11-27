9cbce6273109ff7b4b5c55a73e1e580c95bd6397
Bernie Dante <97004079+dragcotech@users.noreply.github.com>
2025-11-27 22:15:55 +0700

    first commit

--- lua/plugins/configs/ibl.lua
+++ lua/plugins/configs/ibl.lua
@@ -2,16 +2,15 @@ return {
     "lukas-reineke/indent-blankline.nvim",
 
     config = function()
-        -- hiển thị dấu chấm cho space/tab
         vim.opt.list = true
-        vim.opt.listchars:append "lead:·"
-        vim.opt.listchars:append "tab:··"
-        vim.opt.listchars:append "trail:·"
+        -- vim.opt.listchars:append "lead:·"
+        -- vim.opt.listchars:append "tab:··"
+        -- vim.opt.listchars:append "trail:·"
 
         -- setup indent guides
         require("ibl").setup({
             indent = {
-                char = "┆", -- mảnh hơn │
+                char = "┆",
             },
             whitespace = {
                 highlight = { "IblWhitespace" },
@@ -19,8 +18,7 @@ return {
             },
         })
 
-        -- highlight mờ
-        vim.api.nvim_set_hl(0, "IblIndent", { fg = "#444444", nocombine = true })
-        vim.api.nvim_set_hl(0, "IblWhitespace", { fg = "#444444", nocombine = true })
+        vim.api.nvim_set_hl(0, "IblIndent", { fg = "#777777", nocombine = true })
+        vim.api.nvim_set_hl(0, "IblWhitespace", { fg = "#777777", nocombine = true })
     end,
 }
